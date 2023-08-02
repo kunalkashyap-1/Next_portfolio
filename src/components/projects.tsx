e91dcc730eae91a8f03b950b41f566d9a66fcafd
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2023-08-02 17:51:38 +0530

    loader while api call

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,19 +6,23 @@ import { useState, useEffect } from "react";
 
 export default function Projects() {
   const [repos, setRepos] = useState<Response[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     axios
-      .get<Response[]>(
-        "https://gh-pinned-repos.egoist.dev/?username=kunalkashyap-1"
-      )
-      .then((response) => {
-        setRepos(response.data);
-      })
-      .catch((error: any) => {
-        console.error("ERROR: ", error.message);
-        // Handle error if needed
-      });
+    .get<Response[]>(
+      "https://gh-pinned-repos.egoist.dev/?username=kunalkashyap-1"
+    )
+    .then((response) => {
+      setRepos(response.data);
+    })
+    .catch((error: any) => {
+      console.error("ERROR: ", error.message);
+      // Handle error if needed
+    })
+    .finally(()=>{
+      setIsLoading(false);
+    });
   }, []);
 
   return (
@@ -27,19 +31,25 @@ export default function Projects() {
         Projects
       </h1>
       <div className="container mx-auto">
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 my-5">
-          {repos.map((item: Response, i: number) => (
-            <div
-              data-aos="fade-right"
-              data-aos-delay={50 + 100 * i}
-              data-aos-duration={800}
-              key={i}
-              className={`col-span-6 sm:col-span-1 `}
-            >
-              <ProjectsModal key={i} data={item} />
-            </div>
-          ))}
-        </div>
+        {isLoading ? (
+          <div className="flex items-center justify-center h-20">
+            <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-white-900"></div>
+          </div>
+        ) : (
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 my-5">
+            {repos.map((item: Response, i: number) => (
+              <div
+                data-aos="fade-right"
+                data-aos-delay={50 + 100 * i}
+                data-aos-duration={800}
+                key={i}
+                className={`col-span-6 sm:col-span-1 `}
+              >
+                <ProjectsModal key={i} data={item} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
